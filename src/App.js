7c4e4a7ae4258ae9bd4e9e0379409e5bb8adf376
Sebastian Andreasson <sebastian.andreasson93@gmail.com>
2021-06-18 13:59:05 +0200

    space

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import React, { Suspense } from 'react'
 import * as THREE from 'three'
 
-import { FlyControls, Html, OrbitControls } from '@react-three/drei'
+import {
+  FlyControls,
+  Html,
+  OrbitControls,
+  Reflector,
+  Stars,
+} from '@react-three/drei'
 import { Canvas, useFrame } from '@react-three/fiber'
 import { Loader, useGLTF, softShadows } from '@react-three/drei'
 import { useRef } from 'react'
 import styled from 'styled-components'
+import {
+  EffectComposer,
+  Noise,
+  Pixelation,
+  DepthOfField,
+  Vignette,
+  Bloom,
+} from '@react-three/postprocessing'
 
 softShadows()
 
@@ -40,7 +54,35 @@ const Box = () => {
   })
 
   return (
-    <mesh receiveShadow castShadow ref={mesh} position-y={1}>
+    <mesh
+      receiveShadow
+      castShadow
+      ref={mesh}
+      position-y={1}
+      material-roughness={0.75}
+      material-metalness={1}
+    >
+      <boxGeometry />
+      <meshStandardMaterial
+        attach="material"
+        roughness={0}
+        metalness={1}
+        color="orange"
+      />
+    </mesh>
+  )
+}
+
+const AnotherBox = () => {
+  const mesh = useRef()
+
+  useFrame(() => {
+    mesh.current.rotation.x += 0.01
+    mesh.current.rotation.y += 0.005
+  })
+
+  return (
+    <mesh receiveShadow castShadow ref={mesh} position={[-2, 1, 2]} scale={0.5}>
       <boxGeometry />
       <meshStandardMaterial />
     </mesh>
@@ -65,7 +107,7 @@ const Root = styled.div`
   position: relative;
   width: 100%;
   height: 100%;
-  background-color: orange;
+  background-color: black;
 `
 
 const Container = styled.div`
@@ -73,6 +115,7 @@ const Container = styled.div`
   top: 0;
 
   > h1 {
+    color: white;
     margin: 0;
     padding: 1em;
     font-size: 64px;
@@ -85,19 +128,35 @@ const App = () => {
   return (
     <Root>
       <Canvas shadows camera={{ position: [5, 5, 5] }}>
-        <fog attach="fog" args={['orange', 15, 150]} />
-        <FlyControls />
-        {/* <OrbitControls
+        <fog attach="fog" args={['#000', 16, 50]} />
+        <OrbitControls
           // autoRotate
           enablePan={false}
           enableZoom={true}
-          maxPolarAngle={Math.PI / 3}
-          minPolarAngle={Math.PI / 3}
-        /> */}
+          // maxPolarAngle={Math.PI / 3}
+          // minPolarAngle={Math.PI / 3}
+        />
         <ambientLight intensity={0.6} />
         <pointLight castShadow position={[5, 5, 5]} />
-        <Plane />
+        <Stars />
+        {/* <Plane /> */}
         <Box />
+        {/* <AnotherBox /> */}
+        <EffectComposer>
+          {/* <Pixelation granularity={4} /> */}
+          {/* <DepthOfField
+            focusDistance={0}
+            focalLength={0.1}
+            bokehScale={2}
+            height={480}
+          /> */}
+          {/* <Bloom luminanceThreshold={0} luminanceSmoothing={2} height={300} /> */}
+          <Noise opacity={0.02} />
+          <Vignette />
+        </EffectComposer>
+        <gridHelper args={[1000, 1000]}>
+          <meshBasicMaterial color="lime" />
+        </gridHelper>
       </Canvas>
       <Container className="App">
         <h1>Testing three fiber</h1>
